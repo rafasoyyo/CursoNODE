2173c15b0f663251c0ed8ad33a959a5b89768c56
Rafael <rafa_soyyo@hotmail.com>
2016-03-10 23:09:56 +0100

    ejercicios tema 2 y 3

---
+++ TEMA3/ejercicio_3/controllers/data.js
@@ -0,0 +1,10 @@
+
+var nombres = ["Mario", "Patricia", "Manolo", "Santi", "David", "Oscar", "Luis", "Saul", "Autor"];
+
+var getNames = function() {
+	return nombres;
+}
+
+module.exports = {
+	getNames: getNames
+}
---
+++ TEMA3/ejercicio_3/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var data = require('../controllers/Data');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  	res.render('index', { 
+  							title: 'Imagina_todo',
+							nombres : data.getNames()
+	});
+});
+
+module.exports = router;
